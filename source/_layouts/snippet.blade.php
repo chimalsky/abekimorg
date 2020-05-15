91cd97cb3b7d20d3e92ac0cda2ffb320b9c4fdc7
Abraham Kim <gucci@trap.attlocal.net>
2020-05-15 10:15:26 -0400

    starting drafting snippets

---
+++ source/_layouts/snippet.blade.php
@@ -0,0 +1,62 @@
+@extends('_layouts.master')
+
+@push('meta')
+    <meta property="og:title" content="{{ $page->title }}" />
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="{{ $page->getUrl() }}"/>
+    <meta property="og:description" content="{{ $page->description }}" />
+@endpush
+
+@section('body')
+    <header class="w-full">
+        <a href="/">
+            <div class="w-24 h-24 bg-cover bg-right border border-yellow-500"
+                style="background-image: url('/assets/img/joelpeven-landscape.jpg')">
+            </div>
+         </a>
+    </header>
+
+    @if ($page->cover_image)
+        <img src="{{ $page->cover_image }}" alt="{{ $page->title }} cover image" class="mb-2">
+    @endif
+
+    <h1 class="text-6xl mt-0 font-black text-orange-500 tracking-tighter no-underline text-center opacity-25">
+        @foreach (explode(' ', $page->title) as $word)
+            <span class="@if ($loop->last) text-yellow-500 @endif">
+                {{ $word }}
+            </span>
+        @endforeach
+    </h1>
+
+    @if ($page->categories)
+        @foreach ($page->categories as $i => $category)
+            <a
+                href="{{ '/blog/categories/' . $category }}"
+                title="View posts in {{ $category }}"
+                class="inline-block bg-gray-300 hover:bg-blue-200 leading-loose tracking-wide text-gray-800 uppercase text-xs font-semibold rounded mr-4 px-3 pt-px"
+            >{{ $category }}</a>
+        @endforeach
+    @endif
+
+    <div class="mb-10 pb-4 text-xl" v-pre>
+        @yield('content')
+    </div>
+
+    <nav class="flex justify-between text-sm md:text-base">
+        <div>
+            @if ($next = $page->getNext())
+                <a href="{{ $next->getUrl() }}" title="Older Post: {{ $next->title }}">
+                    &LeftArrow; {{ $next->title }}
+                </a>
+            @endif
+        </div>
+
+        <div>
+            @if ($previous = $page->getPrevious())
+                <a href="{{ $previous->getUrl() }}" title="Newer Post: {{ $previous->title }}">
+                    {{ $previous->title }} &RightArrow;
+                </a>
+            @endif
+        </div>
+    </nav>
+@endsection
---
+++ source/_posts/first-snippet.md
@@ -0,0 +1,40 @@
+---
+extends: _layouts.snippet
+section: content
+title: HBO Now
+date: 2020-05-15
+description: Freedom of Feelings
+---
+
+When I was getting into Breaking Bad, I kept hearing about The Sopranos. I vaguely knew that 
+it was set in the mafia world so I immediately casted it off.
+
+Growing up, gangs and mobs have had a huge influence on me.
+
+So it's funny that I casted off The Sopranos because of its mob ties.
+
+I think that was a sign... cause this was back in college. That I was looking for new things.
+Mostly I was looking for a new me.
+
+Maybe my belly knew my tastes better than my head. It immediately casted off The Sopranos as soon as there was any whiff of Mafia. That was what the old Abe would watch.
+
+So about eight years later I'm talking with Joel. I don't know exactly what led to the topic 
+of therapists, but it had something to do with a startup I was working with that dealt with 
+therapists. And so Joel started telling me about The Sopranos main character Tony, who 
+was suffering from PTSD.
+
+Now that I've watched the show I know that everything he does isn't because of the PTSD. 
+But at the time while listening to Joel describe the show, it felt like the entire plot 
+was driven by the past trauma. Like everything Tony was doing in the mob world was 
+driven by latent internal scars.
+
+This description made me want to watch The Sopranos finally. Now that it wasn't tied 
+to the mob at all. It was tied to something else this time. Psychology.
+
+It was only after being engrossed into the show, while ranting my theories derivedd from watching it to Trisha that I began realizing with my head what an influence mafia entertainmentn had had on me.
+
+Frank Sinatra. 50 Cent. Scarface. Future. Gucci Mane. The Godfather.
+
+All of this was connected someway. Everyone above were connected in some patchwork of cultural lineage. The hood life that gangster rap featured took so much from the Italian mafia world.
+
+I'm not a cultural theorist so I can spell this case out exactly, but if you want just concrete evidence you can look for lyrics in rap referring to Frank Sinatra, and Mob Ties and the like.
